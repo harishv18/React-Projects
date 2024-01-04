46c7de4f1d3d1ad7f69b17c046318c44fdc42e5f
harishv27 <harish00411@gmail.com>
2024-01-04 23:20:07 +0530

    My Blog Project

---
+++ my-blog/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
+function App() {
+  return (
+    <div className="App">
+      <h1>My Awesome Blog</h1>
+      <div id="page-body">
+        <HomePage />
+        <AboutPage />
+        <ArticlePage />
+        <ArticlesListPage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
